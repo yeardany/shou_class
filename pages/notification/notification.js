e0567fa75d517da7c2fa42d563081209f1b78e27
euros <kirs@evercx.me>
2019-05-16 11:41:35 +0800

    移除关于页面，首页增加下拉刷新

--- pages/notification/notification.js
+++ pages/notification/notification.js
@@ -15,10 +15,20 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    this.getData(false)
+  },
+
+  onPullDownRefresh: function() {
+    this.getData(true)
+  },
+
+  getData: function(isPullDownRefresh) {
     utils.getDatum(app.globalData.notificationTable, (res) => {
       this.setData({
         notificationList: res.data.objects
       })
+      if (isPullDownRefresh)
+        wx.stopPullDownRefresh()
     })
   }
 })
